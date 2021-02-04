5cf8285d75736ba9167976a2b4bc7bfe1ab0fa21
Matt Gowie <matt@masterpoint.io>
2021-02-04 15:26:41 -0700

    feat: adds an example for the different reference types

---
+++ terraform-fundamentals/examples/syntax-examples/references_example.tf
@@ -0,0 +1,52 @@
+## Resource Attribute Refence
+###############################
+resource "aws_security_group" "firewall" {
+  name = "my-security-group"
+
+  # ...
+}
+
+resource "aws_instance" "example" {
+  # ...
+
+  security_groups = [
+    aws_security_group.firewall.id
+  ]
+}
+
+## Data Source Reference
+##########################
+
+data "aws_vpc" "target_vpc" {
+  id = "vpc-349781234509"
+}
+
+output "vpc_arn" {
+  value       = data.aws_vpc.target_vpc.arn
+  description = "The Amazon Resource Name of the target VPC."
+}
+
+## Module Output Refernece
+############################
+# This `rds_cluster` module defines outputs for `username` + `password`
+module "database" {
+  source = "./rds_cluster"
+  # ...
+}
+
+# Don't ever do this in the real world ;)
+resource "aws_s3_bucket_object" "rds_credentials" {
+  bucket  = "tf-fundamentals-finn-mertens"
+  key     = "rds_credentials.txt"
+  content = "Username: ${module.database.username}\nPassword: ${module.database.password}"
+}
+
+## Provisioner Self Reference
+###############################
+resource "aws_instance" "web" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "echo The server has been created and its IP address is ${self.private_ip}"
+  }
+}
