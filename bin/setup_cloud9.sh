8a043743d79fe4b23899c57f439f059cde112b97
Matt Gowie <matt@masterpoint.io>
2021-03-07 12:16:59 -0700

    feat: adds setup_cloud9.sh script

---
+++ bin/setup_cloud9.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Cloud9 instance updates
+###########################
+
+# Resize our Cloud9 instances to 20GB
+curl -s \
+    https://gist.githubusercontent.com/wongcyrus/a4e726b961260395efa7811cab0b4516/raw/6e70a124c5cb9f6ce5519d8b5b302e8a137e5620/resize.sh |
+    sh
+
+# Install our tools
+####################
+
+# Install jq - a useful tool for querying json documents from the command line.
+sudo yum -y install jq
+
+# Install Terraform - We're installing directly from Hashi, but for future usage I suggest using tfenv: https://github.com/tfutils/tfenv
+curl -O https://releases.hashicorp.com/terraform/0.12.30/terraform_0.12.30_linux_amd64.zip
+sudo unzip terraform_0.12.30_linux_amd64.zip -d /usr/bin/
+
+# Pull Repo
+############
+
+mkdir -p workshop
+cd workshop
+git clone https://github.com/Gowiem/terraform-workshops .
