021c3c967efd2192404701d011884b23781736e0
Matt Gowie <matt@masterpoint.io>
2021-09-22 12:02:06 -0600

    feat: adds protected vpc + subnets to teaching account

--- account-nuke-bomber/main.tf
+++ account-nuke-bomber/main.tf
@@ -2,6 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "vpc" {
+  source      = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
+
+  namespace   = "mp"
+  environment = "ue1"
+  stage       = "teaching"
+  name        = "default"
+  cidr_block  = "10.20.0.0/16"
+
+  tags = { Protected = "true" }
+}
+
+module "subnets" {
+  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
+
+  namespace   = "mp"
+  environment = "ue1"
+  stage       = "teaching"
+  name        = "default"
+
+  availability_zones   = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  vpc_id               = module.vpc.vpc_id
+  igw_id               = module.vpc.igw_id
+  cidr_block           = module.vpc.vpc_cidr_block
+  nat_gateway_enabled  = true
+
+  tags = { Protected = "true" }
+}
+
 module "nuke_bomber" {
   source = "../../../../masterpoint/terraform-aws-nuke-bomber"
   # source  = "masterpointio/nuke-bomber/aws"
