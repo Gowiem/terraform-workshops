fc4cf1eb6c7568cfdf40e5bcc08eeee153c67635
Matt Gowie <matt@masterpoint.io>
2021-09-27 15:13:13 -0600

    fix: updates VPC + Subnet names in account-nuke-bomber

--- account-nuke-bomber/main.tf
+++ account-nuke-bomber/main.tf
@@ -12,8 +12,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
+module "ue1_vpc" {
+  source = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
 
   namespace   = "mp"
   environment = "ue1"
@@ -24,25 +24,25 @@ module "vpc" {
   tags = { Protected = "true" }
 }
 
-module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
+module "ue1_subnets" {
+  source = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
 
   namespace   = "mp"
   environment = "ue1"
   stage       = "teaching"
   name        = "default"
 
-  availability_zones   = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = true
+  availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  vpc_id              = module.ue1_vpc.vpc_id
+  igw_id              = module.ue1_vpc.igw_id
+  cidr_block          = module.ue1_vpc.vpc_cidr_block
+  nat_gateway_enabled = true
 
   tags = { Protected = "true" }
 }
 
-module "vpc" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
+module "ue2_vpc" {
+  source = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
 
   namespace   = "mp"
   environment = "ue2"
@@ -52,35 +52,34 @@ module "vpc" {
 
   tags = { Protected = "true" }
 
-  providers {
+  providers = {
     aws = aws.ohio
   }
 }
 
-module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
+module "ue2_subnets" {
+  source = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
 
   namespace   = "mp"
   environment = "ue2"
   stage       = "teaching"
   name        = "default"
 
-  availability_zones   = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = true
+  availability_zones  = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  vpc_id              = module.ue2_vpc.vpc_id
+  igw_id              = module.ue2_vpc.igw_id
+  cidr_block          = module.ue2_vpc.vpc_cidr_block
+  nat_gateway_enabled = true
 
   tags = { Protected = "true" }
 
-  providers {
+  providers = {
     aws = aws.ohio
   }
 }
 
-
-module "vpc" {
-  source      = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
+module "uw2_vpc" {
+  source = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.27.0"
 
   namespace   = "mp"
   environment = "uw2"
@@ -90,28 +89,28 @@ module "vpc" {
 
   tags = { Protected = "true" }
 
-  providers {
+  providers = {
     aws = aws.oregon
   }
 }
 
-module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
+module "uw2_subnets" {
+  source = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.39.5"
 
   namespace   = "mp"
   environment = "uw2"
   stage       = "teaching"
   name        = "default"
 
-  availability_zones   = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = true
+  availability_zones  = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  vpc_id              = module.uw2_vpc.vpc_id
+  igw_id              = module.uw2_vpc.igw_id
+  cidr_block          = module.uw2_vpc.vpc_cidr_block
+  nat_gateway_enabled = true
 
   tags = { Protected = "true" }
 
-  providers {
+  providers = {
     aws = aws.oregon
   }
 }
